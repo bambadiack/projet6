853928373df873a08047df74548dddf66148ad5e
bambadiack <95110303+bambadiack@users.noreply.github.com>
2023-04-17 15:52:49 +0000

    Add files via upload

---
+++ application de gestion de budget/README.md
@@ -0,0 +1,10 @@
+# semaine 5 - deploiement
+
+
+## Netlify
+
+[https://www.netlify.com/](https://www.netlify.com/)
+
+## Gitub pages
+
+[https://github.com/](https://github.com/)
---
+++ application de gestion de budget/js/app_gb.js
@@ -0,0 +1,4 @@
+let pop = document.getElementById('popRevenu');
+let popButton = document.getElementById('btnAjouRevenupop');
+let closs = document.querySelector('.closs');
+console.log(popButton);
